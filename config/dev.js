7a76ccc6efa638c165f4089d799062df141d57f4
danale <danale@danales-MacBook-Pro.local>
2018-04-21 08:37:58 -0400

    setting up dev and prod config files

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+// dev.js - dont commit this!!!
+module.exports = {
+  googleClientID:
+    '198225304792-pa4auscj7n9el0l1prdu3j8126refois.apps.googleusercontent.com',
+  googleClientSecret: 'QzwCXAd3sRgyV9XTp7Xy25wi',
+  mongoURI: 'mongodb://localhost/server_users',
+  cookieKey: 'B3QY72ZGAtlmFS3oEsLiw2i1'
+};
