3ff00f617b520b74fce43d752a06c9c27944c124
danale <danale@danales-MacBook-Pro.local>
2018-04-21 07:13:18 -0400

    generating production resources

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,7 +10,16 @@ module.exports = app => {
 
   app.get('/auth/google/callback', passport.authenticate('google'));
 
-  app.get('/api/current_user', (req, res) => {
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    // prove to whoever is
+    // making request that
+    // they are no longer signed in
     res.send(req.user);
+  });
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.session);
+    //res.send(req.user);
   });
 };
