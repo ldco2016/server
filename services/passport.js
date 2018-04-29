d123b287d47c159fd1cc033afe65578b7604e86e
danale <danale@danales-MacBook-Pro.local>
2018-04-29 15:06:20 -0400

    Refactored code to use async/await syntax for cleaner code

--- services/passport.js
+++ services/passport.js
@@ -26,22 +26,16 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id })
-        .then(existingUser => {
-          if (existingUser) {
-            // we already have a record with given profile id
-            done(null, existingUser);
-          } else {
-            // we dont have a user record with this id, make a new record
-            new User({ googleId: profile.id })
-              .save()
-              .then(user => done(null, user));
-          }
-        })
-        .catch(err => {
-          console.log(err);
-        });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        // we already have a record with given profile id
+        done(null, existingUser);
+      }
+      // we dont have a user record with this id, make a new record
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
