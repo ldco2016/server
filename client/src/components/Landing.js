aaa58d60202c53264c2bab7e7ab0939856ecbdd1
danale <danale@danales-MacBook-Pro.local>
2018-07-09 16:29:18 -0400

    Installed Stripe with React library resolve #2

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Landing = () => {
   return (
     <div style={{ textAlign: 'center' }}>
-      <h1>Emailer!</h1>
-      Collect feedback from your users
+      <h1>MicroUrbCSA</h1>
+      Get weekly veggies directly to your door!
     </div>
   );
 };
