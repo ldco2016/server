8f9b95e3fb8075880ffc2c83187832c020c8b87c
danale <danale@danales-MacBook-Pro.local>
2018-07-10 14:52:41 -0400

    Setup Stripe env variables with React resolve #3,#4

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
     '198225304792-pa4auscj7n9el0l1prdu3j8126refois.apps.googleusercontent.com',
   googleClientSecret: 'QzwCXAd3sRgyV9XTp7Xy25wi',
   mongoURI: 'mongodb://localhost/server_users',
-  cookieKey: 'B3QY72ZGAtlmFS3oEsLiw2i1'
+  cookieKey: 'B3QY72ZGAtlmFS3oEsLiw2i1',
+  stripePublishableKey: 'pk_test_GJBY8K2I9ALcdoFV6YZPgTe6',
+  stripeSecretKey: 'sk_test_bQmCJExmxmEfrp2LIKCHcu0r'
 };
