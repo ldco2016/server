fd432e068973a5778b45aabf8a3124bc1a31dee5
danale <danale@danales-MacBook-Pro.local>
2018-07-12 09:14:06 -0400

    Redesigned checkout modal and header to be more comprehensive for the user resolve #7

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -5,10 +5,14 @@ class Payments extends Component {
   render() {
     return (
       <StripeCheckout
+        name="MicroUrbCSA"
+        description="$200 for 200 veggie credits"
         amount={20000}
         token={token => console.log(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+        <button className="btn">Add Credits</button>
+      </StripeCheckout>
     );
   }
 }
