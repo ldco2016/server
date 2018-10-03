867fa4d183a0fc5c82deedd0cb1e2480db5910f7
danale <danale@danales-MacBook-Pro.local>
2018-10-02 23:51:26 -0400

    Sendgrid setup

--- config/dev.js
+++ config/dev.js
@@ -6,5 +6,6 @@ module.exports = {
   mongoURI: 'mongodb://localhost/server_users',
   cookieKey: 'B3QY72ZGAtlmFS3oEsLiw2i1',
   stripePublishableKey: 'pk_test_GJBY8K2I9ALcdoFV6YZPgTe6',
-  stripeSecretKey: 'sk_test_bQmCJExmxmEfrp2LIKCHcu0r'
+  stripeSecretKey: 'sk_test_bQmCJExmxmEfrp2LIKCHcu0r',
+  sendGridKey: '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'
 };
