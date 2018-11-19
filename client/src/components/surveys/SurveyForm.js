927839bac234675fdb08ded5ccdf4d886981943f
danale <danale@danales-MacBook-Pro.local>
2018-11-19 12:59:20 -0600

    Implemented persisting form values

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmails';
 
 const FIELDS = [
   { label: 'Survey Title', name: 'title' },
@@ -30,7 +31,7 @@ class SurveyForm extends Component {
   render() {
     return (
       <div>
-        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+        <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
           {this.renderFields()}
           <Link to="/surveys" className="red btn-flat white-text">
             Cancel
@@ -47,11 +48,20 @@ class SurveyForm extends Component {
 
 function validate(values) {
   const errors = {};
-  if (!values.title) {
-    errors.title = 'You must provide a title';
-  }
+
+  errors.emails = validateEmails(values.emails || '');
+
+  _.each(FIELDS, ({ name }) => {
+    if (!values[name]) {
+      errors[name] = 'You must provide a value';
+    }
+  });
 
   return errors;
 }
 
-export default reduxForm({ validate, form: 'SurveyForm' })(SurveyForm);
+export default reduxForm({
+  validate,
+  form: 'SurveyForm',
+  destroyOnUnmount: false
+})(SurveyForm);
