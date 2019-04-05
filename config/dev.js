ff6c72d0b347ce3b1658000b6355697f1b5aeb24
danale <danale@danales-MacBook-Pro.local>
2019-04-05 13:55:16 -0500

    First Jest Test

--- config/dev.js
+++ config/dev.js
@@ -1,12 +1,13 @@
 // dev.js - dont commit this!!!
 module.exports = {
   googleClientID:
-    '198225304792-pa4auscj7n9el0l1prdu3j8126refois.apps.googleusercontent.com',
-  googleClientSecret: 'QzwCXAd3sRgyV9XTp7Xy25wi',
-  mongoURI: 'mongodb://localhost/server_users',
-  cookieKey: 'B3QY72ZGAtlmFS3oEsLiw2i1',
-  stripePublishableKey: 'pk_test_GJBY8K2I9ALcdoFV6YZPgTe6',
-  stripeSecretKey: 'sk_test_bQmCJExmxmEfrp2LIKCHcu0r',
-  sendGridKey: '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',
-  redirectDomain: 'http://localhost:3000'
+    "425355206803-56bj8m77g2vrali7mbb6fufpvj1ao0q0.apps.googleusercontent.com",
+  googleClientSecret: "7a2UhBsgsfOFHf_Lq4JXBn7d",
+  mongoURI: "mongodb://localhost/server_users",
+  cookieKey: "B3QY72ZGAtlmFS3oEsLiw2i1",
+  stripePublishableKey: "pk_test_GJBY8K2I9ALcdoFV6YZPgTe6",
+  stripeSecretKey: "sk_test_bQmCJExmxmEfrp2LIKCHcu0r",
+  sendGridKey:
+    "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",
+  redirectDomain: "http://localhost:3000"
 };
