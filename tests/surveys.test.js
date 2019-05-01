86ba133051a176258122113bbb8b2825ee69b209
danale <danale@danales-MacBook-Pro.local>
2019-04-30 21:17:22 -0500

    Asserting Blog Creation

--- tests/surveys.test.js
+++ tests/surveys.test.js
@@ -11,10 +11,53 @@ afterEach(async () => {
   await page.close();
 });
 
-test("when logged in, can see survey create form", async () => {
-  await page.login();
-  await page.click("a.btn-floating");
+describe("When logged in", async () => {
+  beforeEach(async () => {
+    await page.login();
+    await page.click("a.btn-floating");
+  });
 
-  const label = await page.getContentsOf("form label");
-  expect(label).toEqual("Survey Title");
+  test("can see survey create form", async () => {
+    const label = await page.getContentsOf("form label");
+    expect(label).toEqual("Survey Title");
+  });
+
+  describe("And using valid inputs", async () => {
+    beforeEach(async () => {
+      await page.type("input[name=title]", "My Title");
+      await page.type("input[name=subject]", "My Subject");
+      await page.type("input[name=body]", "This is the body of the email");
+      await page.type("input[name=recipients]", "dancortes@protonmail.com");
+      await page.click("form button.teal");
+    });
+
+    test("Submitting takes user to review screen", async () => {
+      const text = await page.getContentsOf("h5");
+
+      expect(text).toEqual("Please confirm your entries");
+    });
+
+    test("Submitting then saving adds survey to index page", async () => {
+      await page.click("button.green");
+      await page.waitFor(".card");
+
+      const title = await page.getContentsOf("span.card-title");
+      const content = await page.getContentsOf("p");
+
+      expect(title).toEqual("My Title");
+      expect(content).toEqual("This is the body of the email");
+    });
+  });
+
+  describe("And using invalid inputs", async () => {
+    beforeEach(async () => {
+      await page.click("form button.teal");
+    });
+
+    test("the form shows an error message", async () => {
+      const inputError = await page.getContentsOf(".red-text");
+
+      expect(inputError).toEqual("You must provide a value");
+    });
+  });
 });
